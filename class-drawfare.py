f678368279571ecbc00db5288009351ebff15d77
Jonny Dark <jonny@jonnydark.com>
2014-02-18 22:06:28 +0000

    Added class matching regex

--- class-drawfare.py
+++ class-drawfare.py
@@ -21,5 +21,17 @@ def get_header_files(root_directory):
                 header_file_list.append(os.path.join(root,filename))
     return header_file_list
 
+def find_class_declarations_in_file(filename):
+    file_contents = open(filename, 'r').read()
+    match = re.search(
+            r'^(?!enum).*class\b\s(\b[A-Za-z_][A-Za-z_0-9]*\b)\s*[$]?(:[$]?\s*[public|protected|private]\s*[^{]*\s*)?{',
+        file_contents, re.M)
+    if match:
+        print match.groups()
+
+
 # get all header files
 header_files = get_header_files(rootdir)
+
+for filename in header_files:
+    find_class_declarations_in_file(filename)
