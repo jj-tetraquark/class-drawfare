77c22fa534b5a7ee550ec9d67f9828358d41e567
jonnydark <jonny@jonnydark.com>
2014-05-04 11:56:47 +0100

    Tidied up to meet PEP8 standards

--- class-drawfare.py
+++ class-drawfare.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
-
-import sys, os, re
+import sys
+import os
+import re
 from termcolor import cprint, colored
 
 if len(sys.argv) is not 2:
@@ -14,29 +15,32 @@ rootdir = sys.argv[1]
 if not os.path.isdir(rootdir):
     cprint("Error: Not a valid directory", color='red', file=sys.stderr)
 
+
 def get_header_files(root_directory):
     header_file_list = list()
     for root, subdirs, files in os.walk(root_directory):
         for filename in files:
             if re.match(r'[A-Za-z_0-9\-]+\.h', filename):
-                header_file_list.append(os.path.join(root,filename))
+                header_file_list.append(os.path.join(root, filename))
     return header_file_list
 
 
-class Cpp_class:
+class CPP_Class:
     def __init__(self, name, filename, inherits):
         self.name = name
         self.filename = filename
         self.inherits = inherits
 
+
 def find_class_declarations_in_file(filename, classList):
     file_contents = open(filename, 'r').read()
-    matches = re.findall(
-        r'^(?!enum).*class\b\s(\b[A-Za-z_][A-Za-z_0-9]*\b)\s*[$]?(:[$]?\s*[public|protected|private]\s*[^{]*\s*)?{',
-        file_contents, re.M)
+    class_regex = re.compile(
+        r"^(?!enum).*class\b\s(\b[A-Za-z_][A-Za-z_0-9]*\b)\s*[$]?(:[$]?\s*"
+        r"[public|protected|private]\s*[^{]*\s*)?{",
+        re.M)
+    matches = class_regex.finditer(file_contents)
     for match in matches:
-        classList.append(Cpp_class(match[0], filename, match[1]))
-         
+        classList.append(CPP_Class(match.group(1), filename, match.group(2)))
 
 
 # get all header files
@@ -50,4 +54,4 @@ for filename in header_files:
 for cppClass in classList:
     filename = colored(cppClass.filename, 'green')
     print filename + " : ",
-    cprint(cppClass.name, attrs=['bold'])
+    cprint("%s [%s]" % (cppClass.name, cppClass.inherits), attrs=['bold'])
